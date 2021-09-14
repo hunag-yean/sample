c7e0724f73d715b3b8ed1b71fcd89c544e6919d8
hunag-yean <jyhuang@systex.com>
2021-09-14 15:14:46 +0800

    更新

--- .sonarlint/default/.sonartmp_6362685046950326746/eslint-bridge-bundle/package/lib/context.d.ts
+++
@@ -1,7 +0,0 @@
-interface Context {
-    workDir: string;
-    shouldUseTypeScriptParserForJS: boolean;
-}
-export declare function getContext(): Context;
-export declare function setContext(ctx: Context): void;
-export {};
--- .sonarlint/default/.sonartmp_6362685046950326746/eslint-bridge-bundle/package/lib/utils/sensitive-arguments.d.ts
+++
@@ -1,3 +0,0 @@
-import { Rule } from 'eslint';
-import * as estree from 'estree';
-export declare function checkSensitiveCall(context: Rule.RuleContext, callExpression: estree.CallExpression, sensitiveArgumentIndex: number, sensitiveProperty: string, sensitivePropertyValue: boolean, message: string): void;
